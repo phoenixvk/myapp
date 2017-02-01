4a858f5ea16a1f739c3da2ea1d9151d3ec85c4cb
phoenixvk <vaibhav.kadam45@gmail.com>
2017-02-01 11:21:01 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var speakeasy = require('speakeasy');
+
+
+var secret = speakeasy.generateSecret({length: 20});
+console.log(secret.base32); // secret of length 20
+
+
+
+var token = speakeasy.totp({
+  secret: secret.base32,
+  encoding: 'base32'
+});
+
+console.log(token);
+
+var userToken = '121212'; // Verify the token the user gives
+
+var verified = speakeasy.totp.verify({
+  secret: secret.base32,
+  encoding: 'base32',
+  token: userToken
+});
+
+console.log(verified);
